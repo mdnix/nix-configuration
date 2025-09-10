2978e961008d930082722c274f539eb9cef9d808
Marco De Luca <marcodl404@gmail.com>
2025-09-10 21:37:40 +0200

    add disko for x1

---
+++ hosts/x1/disks.nix
@@ -0,0 +1,44 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            swap = {
+              size = "16G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "root";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
