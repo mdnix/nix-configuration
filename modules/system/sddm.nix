ae661c82fd5771e7f3626ffaeb0632795fe29e1f
Marco De Luca <marcodl404@gmail.com>
2025-09-28 21:11:29 +0200

    add sddm styling

--- modules/system/sddm.nix
+++ modules/system/sddm.nix
@@ -2,12 +2,36 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  sddm-astronaut = pkgs.sddm-astronaut.override {
+    themeConfig = {
+      HeaderTextColor = "#ffffff";
+      DateTextColor = "#ffffff";
+      TimeTextColor = "#ffffff";
+      LoginFieldTextColor = "#ffffff";
+      PasswordFieldTextColor = "#ffffff";
+      UserIconColor = "#ffffff";
+      PasswordIconColor = "#ffffff";
+      WarningColor = "#ff6b6b";
+      LoginButtonBackgroundColor = "#1a1a1a";
+      SystemButtonsIconsColor = "#ffffff";
+      SessionButtonTextColor = "#ffffff";
+      VirtualKeyboardButtonTextColor = "#ffffff";
+      DropdownBackgroundColor = "#1a1a1a";
+      HighlightBackgroundColor = "#333333";
+      Background = "/home/marco/.config/wallpapers/wallpaper1.jpg";
+    };
+  };
+in {
   services.displayManager = {
     sddm = {
       enable = true;
       wayland.enable = true;
-      package = pkgs.unstable.kdePackages.sddm;
+      package = pkgs.kdePackages.sddm;
+      extraPackages = [
+        sddm-astronaut
+      ];
+      theme = "sddm-astronaut-theme";
       settings = {
         Wayland.SessionDir = "${
           inputs.hyprland.packages."${pkgs.system}".hyprland
@@ -15,4 +39,6 @@
       };
     };
   };
+
+  environment.systemPackages = [sddm-astronaut];
 }
