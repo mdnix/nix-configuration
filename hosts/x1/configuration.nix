b00d589daf1a89b37fece00da2b5ee67ab948857
Marco De Luca <marcodl404@gmail.com>
2026-06-05 08:50:33 +0200

    bump kernel to 7.0.x

--- hosts/x1/configuration.nix
+++ hosts/x1/configuration.nix
@@ -29,7 +29,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.kernelPackages = pkgs.linuxPackages_7_0;
 
   # Intel graphics (Lunar Lake Arc 140V)
   hardware.graphics = {
