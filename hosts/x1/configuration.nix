48f594d961a0159d1cf3429433d31c59926920a3
Marco De Luca <marcodl404@gmail.com>
2026-07-26 19:43:49 +0200

    flake update, kernel update and change wallpaper

--- hosts/x1/configuration.nix
+++ hosts/x1/configuration.nix
@@ -29,7 +29,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_7_0;
+  boot.kernelPackages = pkgs.linuxPackages_7_1;
 
   # Intel graphics (Lunar Lake Arc 140V)
   hardware.graphics = {
